e6d4e6853dd5fa7d961a4fdd8580a8411a17f084
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-03 22:04:48 +0900

    add about component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
