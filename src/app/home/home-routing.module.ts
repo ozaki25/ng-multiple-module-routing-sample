8d0907fce43fb502379e38e41965f111f8c59521
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-03 22:13:55 +0900

    add redirect settings

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  // { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
 ];
 
