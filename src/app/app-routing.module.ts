1c6c4a0ca5acbb9fbdfd612f9fbec6e7339d5027
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-03 21:47:58 +0900

    remove articles module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
   },
-  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
 ];
 
 @NgModule({
